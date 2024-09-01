18e17782c396905388c4500ee0fbeb871d581d8a
Jenny Fang <jfang5792@gmail.com>
2024-08-31 19:13:18 -0700

    rearranged placement of containers

--- public/index.js
+++ public/index.js
@@ -3,33 +3,15 @@
 import createCommentsContainer from "./components/commentsContainer.js";
 import createImgContainer from "./components/imgContainer.js";
 
-const intializePage = () => {
-    const pageSection = document.createElement('h1');
-    pageSection.className = "title"
-    pageSection.innerText = "Dogstagram";
-    document.body.append(pageSection);
-
-    const img = document.createElement('img')
-    document.body.append(img);
-    fetch ('https://api.thedogapi.com/v1/images/search'
-    ).then(res => {
-        return res.json()
-    }).then(data => {
-        img.src = data[0].url
-    })
-
-}
-
 window.onload = () => {
-    intializePage()
     const main = document.createElement('div');
     main.setAttribute('class', 'main');
 
-    const commentsContainer = createCommentsContainer();
-    main.appendChild(commentsContainer);
-
     const imgContainer = createImgContainer();
     main.appendChild(imgContainer);
+
+    const commentsContainer = createCommentsContainer();
+    main.appendChild(commentsContainer);
 
     document.body.appendChild(main);
 }
